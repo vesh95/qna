92f6e724c8b1e4de75c8a13ed99deec35e5ed61b
vesh95 <vesh95.17@ya.ru>
2020-01-30 19:32:25 +0500

    Added Question#create: action, rspec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  let(:question) { create(:question) }
   let(:questions) { create_list(:question, 3)}
 
+  let(:question) { create(:question)}
+
   describe 'GET #index' do
+
     before { get :index }
 
     it 'populates an array of all questions' do
@@ -17,20 +19,19 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
+    before { get :show, params: { id: question } }
 
     it 'assigns the required question to @question' do
-      get :show, params: { id: question }
       expect(assigns(:question)).to eq question
     end
 
     it 'renders show view' do
-      get :show, params: { id: question }
       expect(response).to render_template :show
     end
   end
 
   describe 'GET #new' do
+    let(:question) { create(:question) }
     before { get :new }
 
     it 'assigns the requested Question to @question' do
@@ -51,6 +52,33 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'render edit view' do
       expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'is valid attributes' do
+      it 'saves a new question in the database' do
+        expect { post :create, params: { question: attributes_for(:question) } }
+               .to change(Question, :count).by(1)
+      end
+
+      it 'redirect to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect do
+          post :create, params: { question: attributes_for(:question, :invalid) }
+        end.to_not change(Question, :count)
+      end
+      it 're-render new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
     end
   end
 end
