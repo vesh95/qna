b03dc03acc539aeb98040f1e2808efbffbf74f3a
vesh95 <vesh95.17@ya.ru>
2020-01-31 14:44:50 +0500

    AnswersController#new implements

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,6 +2,10 @@ class AnswersController < ApplicationController
   before_action :set_question, only: %i[create new]
   before_action :set_answer, only: %i[edit update destroy]
 
+  def new
+    @answer = @question.answers.new
+  end
+
   def create
     @answer = @question.answers.new(answer_params)
     if @answer.save
