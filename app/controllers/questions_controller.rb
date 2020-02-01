56d0c07be5ce69ab7c0a8d95430e4b47d7287eb7
vesh95 <vesh95.17@ya.ru>
2020-02-01 18:45:00 +0500

    Change QuestionsController edit, update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,7 +24,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully updated'
     else
       render :edit
     end
