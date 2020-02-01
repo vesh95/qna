a6644e4e81f6e7286c76e8e091fc8311fccaf908
vesh95 <vesh95.17@ya.ru>
2020-02-01 14:43:48 +0500

    Refactored sreate question spec

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -6,42 +6,33 @@ feature 'User can create question', %q{
   I'd like to be able to ask question
 } do
 
-  given(:user) { User.create!(email: 'user@test', password: '12345678') }
+  given(:user) { create(:user) }
 
-  background { visit new_user_session_path }
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit questions_path
+      click_on 'Ask question'
+    end
 
-  scenario 'Authenticated user asks a question' do
+    scenario 'Asks a question' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text'
+      click_on 'Ask'
 
-    fill_in 'Email', with:  user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+      expect(page).to have_content 'Your question successfully created'
+      expect(page).to have_content 'Test question'
+      expect(page).to have_content 'text text'
+    end
 
-    visit questions_path
-    click_on 'Ask question'
+    scenario 'Asks a question wit errors' do
+      click_on 'Ask'
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text'
-    click_on 'Ask'
-
-    expect(page).to have_content 'Your question successfully created'
-    expect(page).to have_content 'Test question'
-    expect(page).to have_content 'text text'
+      expect(page).to have_content "Title can't be blank"
+    end
   end
 
-  scenario 'Authenticated user asks a question wit errors' do
-    fill_in 'Email', with:  user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
 
-    visit questions_path
-    click_on 'Ask question'
-
-    click_on 'Ask'
-
-    # save_and_open_page
-
-    expect(page).to have_content "Title can't be blank"
-  end
 
   scenario 'Unauthenticated user tries to ask a question' do
     visit questions_path
