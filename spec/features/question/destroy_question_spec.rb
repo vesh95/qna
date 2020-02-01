cdf867fd207559893216596b269f9064d6b72da3
vesh95 <vesh95.17@ya.ru>
2020-02-01 22:46:51 +0500

    Added templates destroy question specs

---
+++ spec/features/question/destroy_question_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+feature 'Delete question' do
+  given(:question) { create(:question) }
+
+  describe 'User not signed in' do
+    given(:user) { create(:user) }
+
+    scenario 'tries destroy question' do
+      visit question_path(question)
+      click_on 'Delete question'
+
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
+  end
+
+  describe 'User signed in' do
+    background do
+      given(:user) { create(:user) }
+      sign_in(user)
+    end
+
+    scenario 'trying to remove someone else\'s question' do
+
+    end
+    scenario 'tries to delete self question'
+  end
+end
