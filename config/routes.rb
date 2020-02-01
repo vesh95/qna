b68c851dd189a7c275f619dbd5716067275bde5b
vesh95 <vesh95.17@ya.ru>
2020-02-01 18:56:46 +0500

    Removed answer new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   resources :questions do
-    resources :answers, shallow: true, except: %i[index show]
+    resources :answers, shallow: true, except: %i[index show new]
   end
 end
