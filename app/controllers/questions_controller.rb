a8c043c37629e96cf70884cf579de4ee1fa60ac6
vesh95 <vesh95.17@ya.ru>
2020-02-01 13:46:31 +0500

    Added Devise autenticate_user! before action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_question, only: %i[show edit update destroy]
 
   def index
