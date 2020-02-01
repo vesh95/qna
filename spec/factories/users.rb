15ef5911f8e05deefe46671dc2a7d6a640d1bb6a
vesh95 <vesh95.17@ya.ru>
2020-02-01 13:44:29 +0500

    Changes User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@mail"
+  end
+
   factory :user do
-    
+    email
+    password { '12345678' }
+    password_conformation { '12345678' }
   end
 end
