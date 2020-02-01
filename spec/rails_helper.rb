cc26aac4351abbf62a1ddea2748e6db5c13bc864
vesh95 <vesh95.17@ya.ru>
2020-02-01 14:43:05 +0500

    Added feature helpes for feature spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers, type: :controller
+  config.include FeatureHelpers, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
