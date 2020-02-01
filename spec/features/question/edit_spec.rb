4eec1ab23faa26b43f9be1176b29b24332e2cabf
vesh95 <vesh95.17@ya.ru>
2020-02-01 18:42:49 +0500

    Implements edit question spec

---
+++ spec/features/question/edit_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+feature 'User can edit question' do
+  given!(:question) { create(:question) }
+  describe 'Authenticated user' do
+    given(:user) { create(:user) }
+
+    background do
+      sign_in(user)
+      visit edit_question_path(question)
+    end
+
+    scenario 'edit question with valid params' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text'
+      click_on 'Update Question'
+
+      expect(page).to have_content 'Your question successfully updated'
+      expect(page).to have_content 'Test question'
+      expect(page).to have_content 'text text'
+    end
+
+    scenario 'edit question with invalid params' do
+      fill_in 'Title', with: ''
+      fill_in 'Body', with: 'text text'
+      click_on 'Update Question'
+
+      expect(page).to have_content 'Title can\'t be blank'
+    end
+  end
+
+  scenario 'Unauthenticated user tries edit question' do
+    visit edit_question_path(question)
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
