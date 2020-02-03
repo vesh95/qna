0dd77e0e6c25f9f1b27147928e682df3d9285c9f
vesh95 <vesh95.17@ya.ru>
2020-02-03 14:50:10 +0500

    Changes create answer spec

--- spec/features/question/create_answer_spec.rb
+++ spec/features/question/create_answer_spec.rb
@@ -14,18 +14,17 @@ feature 'User can send answer for selected question' do
 
     scenario 'tries create answer' do
       expect(page).to have_content('Your question successfully created')
-      expect(page).to have_content(question.reload.answers.last.body)
+      expect(page).to have_content('MyText')
     end
   end
 
   describe 'Unauthenticated user' do
     background do
       visit question_path(question)
-      fill_in 'Body', with: 'Answer1'
-      click_on 'Create Answer'
     end
-    scenario 'tries create answer' do
-      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+
+    scenario 'page has not form' do
+      expect(page).to_not have_button("Create Answer")
     end
   end
 
