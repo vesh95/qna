f029c68c715339eb2e0cf12f3b220bcadc03881c
vesh95 <vesh95.17@ya.ru>
2020-02-04 00:51:07 +0500

    Changed question index spec

--- spec/features/question/index_spec.rb
+++ spec/features/question/index_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 feature 'User can seeing all questions' do
-  given!(:question) { create_list(:question, 2, :list_of_questions) }
+  given!(:questions) { create_list(:question, 2, :list_of_questions) }
 
   scenario 'User visit questions list' do
     visit questions_path
-    2.times do |n|
-      expect(page).to have_content("Question#{n+1}")
+    questions.each do |qusetion|
+      expect(page).to have_content(qusetion.title)
     end
   end
 end
