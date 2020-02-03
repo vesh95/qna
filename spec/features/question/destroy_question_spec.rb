c22858df3d9276e648f4740058aa8339f91d64ac
vesh95 <vesh95.17@ya.ru>
2020-02-03 16:15:28 +0500

    Dont'show destroy question links to guest and not authors

--- spec/features/question/destroy_question_spec.rb
+++ spec/features/question/destroy_question_spec.rb
@@ -7,9 +7,8 @@ feature 'Delete question' do
 
     scenario 'tries destroy question' do
       visit question_path(question)
-      click_on 'Delete question'
 
-      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      expect(page).to_not have_link 'Delete question'
     end
   end
 
@@ -18,11 +17,9 @@ feature 'Delete question' do
 
     scenario 'trying to remove someone else\'s question' do
       sign_in(user)
-
       visit question_path(question)
-      click_on 'Delete question'
 
-      expect(page).to have_content 'You can\'t modified this question'
+      expect(page).to_not have_link 'Delete question'
     end
 
     scenario 'tries to delete self question' do
