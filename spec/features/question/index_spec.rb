6eac7c37e37bf6175cf371ac1e9a6b57dec03ca2
vesh95 <vesh95.17@ya.ru>
2020-02-03 16:37:13 +0500

    Changed questions count in index spec

--- spec/features/question/index_spec.rb
+++ spec/features/question/index_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 
 feature 'User can seeing all questions' do
-  # QUESTION: given воссоздает новый список для каждого сценария или он всегда остается один?
-  given!(:question) { create_list(:question, 3) }
+  given!(:question) { create_list(:question, 2) }
 
   scenario 'User visit questions list' do
     visit questions_path
-    expect(page).to have_content('MyString', count: 3)
+    expect(page).to have_content('MyString', count: 2)
   end
 end
