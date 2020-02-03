35832dc00e86326d0fb8513253f1fae247ed3e0c
vesh95 <vesh95.17@ya.ru>
2020-02-03 11:35:52 +0500

    user model authority spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,31 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'is author' do
+    let(:user) { create(:user) }
+
+    context 'of question' do
+      let(:question) { create(:question) }
+
+      it 'author' do
+        expect(question.user.author?(question)).to eq true
+      end
+      it 'not author' do
+        expect(user.author?(question)).to eq false
+      end
+    end
+
+    context 'of answer' do
+      let(:answer) { create(:answer) }
+
+      it 'author' do
+        expect(answer.user.author?(answer)).to eq true
+      end
+
+      it 'not author' do
+        expect(user.author?(answer)).to eq false
+      end
+    end
+  end
 end
