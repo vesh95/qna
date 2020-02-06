204e4a5eb0212032c77e22bee02196919089721c
vesh95 <vesh95.17@ya.ru>
2020-02-06 19:39:57 +0500

    Added best attribute to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,12 @@
 class Answer < ApplicationRecord
+
+  default_scope { order('best DESC, created_at') }
+
   belongs_to :user
   belongs_to :question
 
   validates :body, presence: true
+  validates :best, :inclusion => { in: [true, false] }
+
+  scope :best, -> { where(best: true) }
 end
