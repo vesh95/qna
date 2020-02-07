d6b7cf4823deb993a1e2ba41b5c4f76843b69804
vesh95 <vesh95.17@ya.ru>
2020-02-07 19:57:51 +0500

    Addede specs to create question with attached file

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -25,6 +25,17 @@ feature 'User can create question', %q{
       expect(page).to have_content 'text text'
     end
 
+    scenario 'asks a question with attached file' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Ask'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
     scenario 'Asks a question wit errors' do
       click_on 'Ask'
 
