ebfe38d6e9385333f33080d7a8e592186faf4e1a
vesh95 <vesh95.17@ya.ru>
2020-02-07 10:25:05 +0500

    Added make_best! answer spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :user}
 
   it { should validate_presence_of :body }
+
+  context '#make_best' do
+    let(:question) { create(:question) }
+    let!(:answer1) { create(:answer, question: question) }
+    let(:answer2) { create(:answer, question: question) }
+
+    before { answer1.make_best! }
+
+    it 'makes answer the best' do
+      expect(answer1.reload).to be_best
+      expect(answer2.reload).to_not be_best
+    end
+
+    it 're-make_best answer' do
+      answer2.make_best!
+      expect(answer2.reload).to be_best
+      expect(answer1.reload).to_not be_best
+    end
+  end
 end
