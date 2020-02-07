2b699e668adc82a8bd0697f80ada0be1af011577
vesh95 <vesh95.17@ya.ru>
2020-02-07 09:31:01 +0500

    Refactored js files

--- app/javascript/utilities/answers/form_toggler.js
+++ app/javascript/utilities/answers/form_toggler.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
-  $('.answers').on('click', '.edit-answer-link', function(e) {
-    e.preventDefault()
+  $('.answers').on('click', '.edit-answer-link', function(event) {
+    event.preventDefault()
     let answerId = $(this).data('answerId')
 
     let r = $(`.answer[data-answer-id=${answerId}] .edit`).toggleClass('hidden')
