154e19cb2f550e421f3990cd8957686b7dff97d3
vesh95 <vesh95.17@ya.ru>
2020-02-09 21:03:48 +0500

    Refactored AttachmentsController

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,19 +1,13 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_attachment
 
   def destroy
+    @attachment = ActiveStorage::Attachment.find(params[:id])
+
     if current_user.author?(@attachment.record)
       @attachment.purge
     else
       head 403
     end
   end
-
-  private
-
-  def set_attachment
-    @attachment = ActiveStorage::Attachment.find(params[:id])
-  end
-
 end
