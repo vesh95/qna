11e7923a06b670101c56759b16c1033e33b96cd6
vesh95 <vesh95.17@ya.ru>
2020-02-15 14:08:22 +0500

    Refactored linkable spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it_behaves_like 'linkable'
+
   it { should belong_to :question}
   it { should belong_to :user}
 
-  it { should have_many(:links).dependent(:destroy) }
-
   it { should validate_presence_of :body }
-
-  it { should accept_nested_attributes_for :links }
 
   it 'have many attached files' do
     expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
---
+++ spec/support/shared_examples.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+RSpec.shared_examples "linkable" do
+  it { should have_many(:links).dependent(:destroy) }
+  it { should accept_nested_attributes_for :links }
+end
