e52a26e71c97eb506b9fc16e47fc14143dedb4f8
vesh95 <vesh95.17@ya.ru>
2020-02-17 21:38:23 +0500

    Implements Answer rating

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  include Voted
+
   before_action :authenticate_user!, except: %i[show]
   before_action :set_question, only: %i[create new]
   before_action :set_answer, only: %i[edit update destroy best]
