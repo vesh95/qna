9364e2f1b4844933c12c41305be1709c39ff4b4b
vesh95 <vesh95.17@ya.ru>
2020-02-18 11:18:04 +0500

    Added uniq validation to Vote model, changed #rate inclusion valitation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  validates :rate, numericality: { only_integer: true }
+  validates :user, uniqueness: { scope: [:votable_id, :votable_type] }
+  validates :rate, inclusion: { in: [1, -1] }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,5 +4,14 @@ RSpec.describe Vote, type: :model do
   it { should belong_to :user }
   it { should belong_to :votable }
 
-  it { should validate_numericality_of(:rate).only_integer }
+  it { should validate_inclusion_of(:rate).in_array([1, -1]) }
+
+  let(:user) { create(:user) }
+  let(:question) { create(:question) }
+  let!(:vote) { create(:vote, user: user, votable: question) }
+
+  it 'should validate uniquesness of vote' do
+    vote = Vote.new(user: user, rate: 1, votable: question)
+    expect(vote).not_to be_valid
+  end
 end
