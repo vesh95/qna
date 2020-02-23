56dca88679e7795bf8689c2e56961888e3cc5b8f
vesh95 <vesh95.17@ya.ru>
2020-02-23 21:42:29 +0500

    Fixed remove subscriptions onload

--- app/javascript/channels/questions_channel.js
+++ app/javascript/channels/questions_channel.js
@@ -1,10 +1,18 @@
 import consumer from "./consumer"
 
+var questionsChannel
+
 $(document).on('turbolinks:load', function () {
   var $questionsList = $('#questions-list')
-  consumer.subscriptions.create("QuestionsChannel", {
-    received(data) {
-      $questionsList.append(data.data)
+  if ($questionsList.length) {
+    questionsChannel = consumer.subscriptions.create("QuestionsChannel", {
+      received(data) {
+        $questionsList.append(data.data)
+      }
+    }) // subscriptions.create()
+  } else {
+    if (questionsChannel) {
+      questionsChannel.unsubscribe()
     }
-  })
-})
+  }
+}) // on 'turbolinks:load'
