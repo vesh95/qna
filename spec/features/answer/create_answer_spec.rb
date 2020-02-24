57d5e1a5dcbc0116f76e8b9d6e032986b1b58fea
vesh95 <vesh95.17@ya.ru>
2020-02-24 11:58:45 +0500

    Implements create Answer cable spec

--- spec/features/answer/create_answer_spec.rb
+++ spec/features/answer/create_answer_spec.rb
@@ -50,4 +50,27 @@ feature 'User can send answer for selected question' do
       expect(page).to_not have_button("Create Answer")
     end
   end
+
+  context "mulitple sessions" do
+    scenario "answers appears on another user's page", js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        fill_in 'Body', with: 'test text'
+        click_on 'Create Answer'
+        expect(page).to have_content 'test text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'test text'
+      end
+    end
+  end
 end
