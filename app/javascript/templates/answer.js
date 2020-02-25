2e67a651b525b5b5ec44ae73dd43b55d46adf6c8
vesh95 <vesh95.17@ya.ru>
2020-02-25 11:58:17 +0500

    Added comments to answer that's append by channel

--- app/javascript/templates/answer.js
+++ app/javascript/templates/answer.js
@@ -1,10 +1,12 @@
 export default function answerHtml(data) {
   return `
-    <div class="answer" data-answer-id="${data.data.answer.id}">
+    <div class="answer card" data-answer-id="${data.data.answer.id}">
       ${data.data.answer.body}
       <div class="actions">
         <a href="/answers/${data.data.answer.id}" data-remote="true">Load</a>
       </div>
+      <h6>Comments</h6>
+      <div class="comments" id="answer-comments-${data.data.answer.id}">
     </div>
   `
 }
