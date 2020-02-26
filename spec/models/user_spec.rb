34707b3ca55f131ccbf82a8ffc0ac294cd99e960
vesh95 <vesh95.17@ya.ru>
2020-02-26 19:05:42 +0500

    Implements specs for service, user and authorization units

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,23 @@ RSpec.describe User, type: :model do
   it { should have_many(:questions) }
   it { should have_many(:answers) }
   it { should have_many(:awards) }
+  it { should have_many(:authorizations).dependent(:destroy) }
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123456') }
+    let(:service) { double('Services::FindForOauth') }
+
+    it 'calls Services::FindForOauth' do
+      expect(Services::FindForOauth).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      User.find_for_oauth(auth)
+    end
+  end
 
   describe '#voted?' do
     let(:user) { build(:user) }
