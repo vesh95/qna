03efe738d177ccbd6f423f693ac3e5bc5c190b14
vesh95 <vesh95.17@ya.ru>
2020-02-27 16:26:40 +0500

    Fixed Oauth Authorization spec

--- spec/features/authorization/sign_in_oauth_spec.rb
+++ spec/features/authorization/sign_in_oauth_spec.rb
@@ -17,6 +17,10 @@ feature 'User can sign in with oauth' do
       expect(page).to have_content 'Sign in with Vkontakte'
 
       click_on 'Sign in with Vkontakte'
+      fill_in 'Email', with: 'vk@vk'
+      click_on 'Confirm'
+      open_email 'vk@vk'
+      current_email.click_link 'Confirm my account'
 
       expect(page).to have_content("vk@vk")
       expect(page).to have_content("Sign Out")
@@ -24,9 +28,9 @@ feature 'User can sign in with oauth' do
 
     scenario "can handle authentication error" do
       OmniAuth.config.mock_auth[:github] = :invalid_credentials
-      page.should have_content("Sign in with GitHub")
-      click_link "Sign in"
-      page.should have_content(/because "Invalid credentials"/)
+      expect(page).to have_content("Sign in with GitHub")
+      click_link "Sign in with GitHub"
+      expect(page).to have_content(/because "Invalid credentials"/)
     end
   end
 end
