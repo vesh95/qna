8847e0c456528ecebb695bbfbae40c2f6e0345dd
vesh95 <vesh95.17@ya.ru>
2020-02-27 10:54:43 +0500

    Implements oauth feature specs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,3 +46,14 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
+
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+    'provider' => 'github',
+    'uid' => '11',
+    'info' => {
+      'email' => 'gituser@github',
+      'name' => 'mockuser',
+      'image' => 'mockuser_avatar_url'
+    }
+  })
---
+++ spec/features/authorization/sign_in_oauth_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User can sign in with oauth' do
+  context 'can sign in user' do
+    background { visit new_user_session_path }
+
+    scenario 'throuth Github' do
+      expect(page).to have_content 'Sign in with GitHub'
+
+      click_on 'Sign in with GitHub'
+
+      expect(page).to have_content("gituser")
+      expect(page).to have_content("Sign Out")
+    end
+
+    scenario "can handle authentication error" do
+      OmniAuth.config.mock_auth[:github] = :invalid_credentials
+      page.should have_content("Sign in with GitHub")
+      click_link "Sign in"
+      page.should have_content(/because "Invalid credentials"/)
+    end
+  end
+end
