6f451e5f1f1422921a43bdde4bcff8ebed1a0201
vesh95 <vesh95.17@ya.ru>
2020-03-07 18:56:57 +0500

    Added body checks to unauthorized shared spec

--- spec/support/shared/api_sared_examples.rb
+++ spec/support/shared/api_sared_examples.rb
@@ -3,11 +3,13 @@ shared_examples_for 'API Authorizable' do
     it 'returns 401 status if there is no access_token' do
       do_request(method, api_path, headers: headers, params: request_params ||= nil)
       expect(response.status).to eq 401
+      expect(response.body).to be_empty
     end
 
     it 'returns 401 status if access_token is invalid' do
       do_request(method, api_path, params: {access_token: '1234'}, headers: headers)
       expect(response.status).to eq 401
+      expect(response.body).to be_empty
     end
   end # unauthorized
 end
