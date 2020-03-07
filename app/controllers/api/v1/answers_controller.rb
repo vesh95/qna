aac5dbffc8a75291f161072500887749e34b92bd
vesh95 <vesh95.17@ya.ru>
2020-03-07 23:13:04 +0500

    Changed serializer for @answers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AnswersController < Api::V1::BaseController
 
   def index
     @answers = @question.answers
-    render json: @answers, each_serializer: AnswersSerializer, include: 'user'
+    render json: @answers, include: 'user'
   end
 
   def show
