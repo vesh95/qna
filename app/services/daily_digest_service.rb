7216a67a134520a654c78ee4f7d82302507487a9
vesh95 <vesh95.17@ya.ru>
2020-03-10 15:23:02 +0500

    Fixed dailily digest withot day questions

--- app/services/daily_digest_service.rb
+++ app/services/daily_digest_service.rb
@@ -1,6 +1,8 @@
 class DailyDigestService
   def send_digest
     @questions = Question.where(created_at: 1.day.ago.all_day)
+    return if @questions.empty?
+
     User.find_each do |user|
       DailyDigestMailer.digest(user, @questions)
     end
--- spec/services/daily_digest_spec.rb
+++ spec/services/daily_digest_spec.rb
@@ -2,10 +2,29 @@ require 'rails_helper'
 
 RSpec.describe DailyDigestService do
   let(:users) { create_list(:user, 2) }
-  let!(:questions) { create_list(:question, 3, created_at: 1.day.ago, user: users.last) }
 
-  it 'sends daily digest to all users' do
-    users.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user, questions).and_call_original }
-    subject.send_digest
+  context 'questions exists' do
+    let!(:questions) { create_list(:question, 2, created_at: 1.day.ago, user: users.last) }
+
+    it 'sends daily digest to all users' do
+      users.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user, questions).and_call_original }
+      subject.send_digest
+    end
+  end
+
+  context 'without today questions' do
+    let!(:questions) { create_list(:question, 2, created_at: 4.day.ago, user: users.last) }
+
+    it 'sends daily digest to all users' do
+      expect(DailyDigestMailer).to_not receive(:digest)
+      subject.send_digest
+    end
+  end
+
+  context 'questions not exists' do
+    it 'sends daily digest to all users' do
+      expect(DailyDigestMailer).to_not receive(:digest)
+      subject.send_digest
+    end
   end
 end
