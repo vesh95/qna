9e05f229b9a91cb6600afbf1b5768b82ddbda860
vesh95 <vesh95.17@ya.ru>
2020-03-10 23:39:05 +0500

    Fixed nil class expection without subscriber user

--- spec/services/new_answer_digest_spec.rb
+++ spec/services/new_answer_digest_spec.rb
@@ -4,16 +4,15 @@ RSpec.describe NewAnswerDigestService do
   let!(:answer) { create(:answer, question: question) }
   let(:question) { create(:question) }
   let!(:subscriptions) { create_list(:subscription, 2, question: question)}
+  let!(:another_user) { create(:user) }
 
-  it 'sends answer digest to all users' do
+  it 'sends answer digest to all subscribed users only' do
     Subscription.includes(:user).all.each do |subscription|
       expect(NewAnswerDigestMailer).to receive(:send_notification).with(subscription.user, answer).and_call_original
     end
-    subject.digest(answer)
-  end
 
-  it 'sends answer only subscribed users' do
-    expect(NewAnswerDigestMailer).to receive(:send_notification).with(any_args).thrice.and_call_original
+    expect(NewAnswerDigestMailer).to_not receive(:send_notification).with(another_user, answer).and_call_original
+
     subject.digest(answer)
   end
 end
