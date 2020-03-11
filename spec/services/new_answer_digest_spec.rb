d4b09364cea90e368aea591f832ffb550245aa2f
vesh95 <vesh95.17@ya.ru>
2020-03-11 12:52:44 +0500

    Added expect with another subscribers to new answer digest service

--- spec/services/new_answer_digest_spec.rb
+++ spec/services/new_answer_digest_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe NewAnswerDigestService do
   let(:question) { create(:question) }
   let!(:subscriptions) { create_list(:subscription, 2, question: question)}
   let!(:another_user) { create(:user) }
+  let!(:another_subscriber) { create(:user, subscriptions: create_list(:subscription, 1)) }
 
   it 'sends answer digest to all subscribed users only' do
-    Subscription.includes(:user).all.each do |subscription|
+    Subscription.includes(:user).where(question: question).each do |subscription|
       expect(NewAnswerDigestMailer).to receive(:send_notification).with(subscription.user, answer).and_call_original
     end
 
     expect(NewAnswerDigestMailer).to_not receive(:send_notification).with(another_user, answer).and_call_original
+    expect(NewAnswerDigestMailer).to_not receive(:send_notification).with(another_subscriber, answer).and_call_original
 
     subject.digest(answer)
   end
