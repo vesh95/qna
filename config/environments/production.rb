7b26db1a83b4ad4615283a9bdad11d81add8bdbd
vesh95 <vesh95.17@ya.ru>
2020-03-17 20:26:57 +0500

    Changed sendmail config & Changed credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: '194.61.1.61'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'vesh95.17',
+    password:             Rails.application.credentials[Rails.env.to_sym][:gmail],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
